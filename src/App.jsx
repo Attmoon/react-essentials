ecd7ecded7c4da8fe991df6e533bb4ff4c9b2c05
Attmoon <99157555+Attmoon@users.noreply.github.com>
2024-07-23 20:40:17 +0900

    조건부 content 렌더링

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import TabButton from './components/TabButton.jsx';
 import { EXAMPLES } from './data.js';
 
 function App() {
-  const [ selectedTopic, setSelectedTopic ] = useState('components');
+  const [ selectedTopic, setSelectedTopic ] = useState();
   
 
   function handleSelect(selectedButton) {
@@ -18,6 +18,18 @@ function App() {
 
   console.log('APP COMPONENT EXECUTING');
 
+  let tabContent = <p>Please select a topic.</p>;
+  if(selectedTopic) {
+    tabContent = <div id="tab-content">
+    <h3>{EXAMPLES[selectedTopic].title}</h3>
+    <p>{EXAMPLES[selectedTopic].description}</p>
+    <pre>
+      <code>
+        {EXAMPLES[selectedTopic].code}
+      </code>
+    </pre>
+  </div>       
+  }
   return (
     <div>
       <Header />
@@ -42,16 +54,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-                {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-          </div>
+          {tabContent}
         </section>
         <h2>Time to get started!</h2>
       </main>
